34d4a29b1af2b89382a3a611052a0dfe55daab20
Alex880326 <124200830+Alex880326@users.noreply.github.com>
2023-02-19 12:06:01 +0300

    дз#3

---
+++ №19.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+System.Console.WriteLine("Введите пятизначное число " );
+string? number = Console.ReadLine();
+int firstNum = Convert.ToInt32(Convert.ToString(number![0]));
+int fivthNum = Convert.ToInt32(Convert.ToString(number![4]));
+if (number!.Length < 5 || number!.Length > 5)
+{
+        System.Console.WriteLine("Вы ввели число содержащее больше или меньше пяти цифр.");
+}
+else
+    {
+     if (firstNum == fivthNum)
+    {
+        System.Console.WriteLine("Поздравляю, Ваше число является палиндромом!");
+    }
+     if (firstNum != fivthNum)
+        System.Console.WriteLine("К сожалению, Ваше число не является палиндромом!");
+    }
---
+++ №23.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+System.Console.WriteLine("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+int N = number % 10;
+if (number < 99)
+{
+    System.Console.WriteLine("Отсутствует наличие третьего числа, попробуйте снова ");
+}
+
+else
+{
+    while (number > 999)
+    
+     {
+        number = number/10;
+        }
+        number =number%10;
+        System.Console.WriteLine(number);
+    
+}
