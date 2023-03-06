53a836415a71e2f2e4a0228e8ef1c90e537211f7
Alex880326 <124200830+Alex880326@users.noreply.github.com>
2023-03-06 15:30:38 +0300

    47 50 52

---
+++ №50.cs
@@ -0,0 +1,66 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1 7 -> такого числа в массиве нет
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] GenerateMatrix(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    Random rand = new Random();
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matrix[i, j] = rand.Next(0, 10);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write(matrix[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+        for (int j = 0; j < n; j++)
+                {
+                    a[n+1, j] = s[j];
+    }
+    Console.WriteLine("Введите координаты строки, а затем столбца искомого элемента: ");
+    int a = Convert.ToInt32(Console.ReadLine());
+    int b = Convert.ToInt32(Console.ReadLine());
+    if (a < matrix.GetLength(0) && b < matrix.GetLength(1))
+    {
+        Console.WriteLine($"Внимание, значение указанного Вами элемента: {matrix[a - 1, b - 1]}");
+    }
+    else
+    {
+
+        Console.WriteLine("Внимание, элемент указанный Вами отутствует в данной таблице, повторите ввод");
+        if (int.TryParse(Console.ReadLine(), out a))
+            if (int.TryParse(Console.ReadLine(), out b))
+            {
+                // ...
+            }
+    }
+
+}
+
+int m = ReadInt("Введите количетсво строк матрицы: ");
+int n = ReadInt("Введите количество столбцов матрицы: ");
+var myMatrix = GenerateMatrix(m, n);
+PrintMatrix(myMatrix);
+
---
+++ №52.cs
@@ -0,0 +1,61 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] GenerateMatrix(int m, int n)
+{
+    int[,] matrix = new int [m, n];
+    Random rand = new Random();
+    for(int i = 0; i < m; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            matrix[i,j] = rand.Next(new Random().Next(0,10));
+        }
+    }
+    return matrix;
+}
+void PrintMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write(matrix[i,j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+double[] Mean(int[,] matrix, int n)
+{
+ double[] z = new double[n];
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        double num = 0;
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            num = (num + matrix[i, j]);
+        }
+        num = num / n;
+        z[j] = Math.Round(num,2);
+    }
+    return z;
+}
+
+int m = ReadInt("Введите количетсво строк матрицы: ");
+int n = ReadInt("Введите количество столбцов матрицы: ");
+var myMatrix = GenerateMatrix(m, n);
+double[] z =  Mean(myMatrix, n);
+PrintMatrix(myMatrix);
+System.Console.WriteLine("Среднее арифметическое каждого столбца:");
+System.Console.Write(string.Join("\t",(z)));
